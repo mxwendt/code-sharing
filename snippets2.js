1dc94d43223fc6b0334faf98a739684379ce2034
Max Wendt <mxwendt@gmail.com>
2016-04-28 12:08:18 +0200

    Added fibonacci example 2

---
+++ snippets2.js
@@ -0,0 +1,36 @@
+var snippets = [
+  {
+    "code": "/**\n * @param {Number} size\n * @return {Array} result\n */\nfunction fibonacci(size) { var first = 0; var second = 1; var next; var count = 2; var result = [first, second]; if (size < 2) { return 'the request was made but it was not good'; } while (count++ < size) { next = first + second; first = second; second = next; result.push(next); } return result; }\n\nfibonacci(6);",
+    "execution": [
+      {line: 22, val: "()"},
+      {line: 5, val: "0"},
+      {line: 6, val: "1"},
+      {line: 7, val: "u"},
+      {line: 8, val: "2"},
+      {line: 9, val: "[0,1]"},
+      {line: 10, val: "f"},
+      {line: 13, val: "t"},
+      {line: 14, val: "1"},
+      {line: 15, val: "1"},
+      {line: 16, val: "1"},
+      {line: 17, val: "[...,1]"},
+      {line: 13, val: "t"},
+      {line: 14, val: "2"},
+      {line: 15, val: "1"},
+      {line: 16, val: "2"},
+      {line: 17, val: "[...,2]"},
+      {line: 13, val: "t"},
+      {line: 14, val: "3"},
+      {line: 15, val: "2"},
+      {line: 16, val: "3"},
+      {line: 17, val: "[...,3]"},
+      {line: 13, val: "t"},
+      {line: 14, val: "5"},
+      {line: 15, val: "3"},
+      {line: 16, val: "5"},
+      {line: 17, val: "[...,5]"},
+      {line: 13, val: "f"},
+      {line: 19, val: "[0,1,1,2,3,5]"}
+    ]
+  }
+];
---
+++ visualize2.js
@@ -0,0 +1,70 @@
+window.onload = function() {
+  for (var i = 0; i < snippets.length; i++) {
+    visualize(snippets[i]);
+  }
+};
+
+function visualize(snippet) {
+  var execution = snippet.execution;
+  var code = snippet.code;
+
+  var snippetContainerElem = document.createElement('div');
+  snippetContainerElem.classList.add('snippet');
+
+  var codeContainerElem = document.createElement('div');
+  codeContainerElem.classList.add('code');
+
+  var dataContainerElem = document.createElement('div');
+  dataContainerElem.classList.add('data');
+
+  var codeElem = document.createElement('code');
+  codeElem.textContent = js_beautify(code);
+
+  var preElem = document.createElement('pre');
+  preElem.classList.add('default', 'linenums', 'prettyprint');
+
+  preElem.appendChild(codeElem);
+  codeContainerElem.appendChild(preElem);
+  snippetContainerElem.appendChild(codeContainerElem);
+  snippetContainerElem.appendChild(dataContainerElem);
+  document.querySelector('.snippets').appendChild(snippetContainerElem);
+
+  prettyPrint();
+
+  var w = (execution.length + 2) * 20;
+  var h = codeContainerElem.clientHeight;
+  var lineCount = codeContainerElem.querySelector('ol.linenums').children.length;
+
+  var svg = d3.select(dataContainerElem)
+    .append('svg')
+      .attr('width', w)
+      .attr('height', h)
+      .style('background-image', 'repeating-linear-gradient(180deg, #F8F8F8, #F8F8F8 ' +
+        (h / lineCount) + 'px, #FFFFFF ' + (h / lineCount) + 'px, #FFFFFF ' + (h / lineCount * 2) + 'px)');
+
+  var x = d3.scale.ordinal().domain(d3.range(execution.length + 2)).rangePoints([0, w]);
+  var y = d3.scale.ordinal().domain(d3.range(lineCount + 1)).rangePoints([0, h]);
+
+  var axis = d3.svg.axis()
+    .scale(x)
+    .orient('top')
+    .ticks(2);
+
+  var xAxis = svg.append("g")
+    .attr("class", "axis")
+    .call(axis);
+
+  var dots = svg.selectAll('.val')
+      .data(execution)
+    .enter().append('text')
+      .attr('class', 'val')
+      .attr('x', function(d, i) { return x(i + 1); })
+      // .attr('y', 10)
+      .attr('text-anchor', 'middle')
+      .attr('y', function(d, i) { return y(d.line + 1) - 5; })
+      .text(function(d) { return d.val; });
+
+  // TODO: Add lines between dots to make order more clear?
+
+  snippetContainerElem.style.width = codeContainerElem.clientWidth + dataContainerElem.clientWidth + 1 + 'px';
+}
