1b39acf0a814af3864876372548b55dd43796d35
Max Wendt <mxwendt@gmail.com>
2016-04-28 18:13:20 +0200

    Changed js object notation

--- visualize.js
+++ visualize.js
@@ -5,107 +5,117 @@ window.onload = function() {
 };
 
 function Snippet() {
+  var snippet;
+  var svg;
+  var x;
+  var y;
+  var xAxis;
+  var wrapper;
+  var codeWrapper;
+  var dataWrapper
+  var stateWrapper;
+  var code;
 
-  this.init = function(snippet) {
-    this.snippet = snippet;
-
-    this.createMarkup();
-    this.addCode();
-    this.addData();
-    this.addState();
-    // this.addListeners();
-    this.visualize();
+  return {
+    init: init
   };
 
-  this.visualize = function() {
-    this.svg.attr('width', this.getW());
+  function init(s) {
+    snippet = s;
 
-    this.x = d3.scale.ordinal().domain(d3.range(this.snippet.execution.length + 2)).rangePoints([0, this.getW()]);
-    this.y = d3.scale.ordinal().domain(d3.range(this.getLineCount() + 1)).rangePoints([0, this.getH()]);
+    createMarkup();
+    addCode();
+    addData();
+    addState();
+    // addListeners();
+    visualize();
+  }
+
+  function visualize() {
+    svg.attr('width', getW());
+
+    x = d3.scale.ordinal().domain(d3.range(snippet.execution.length + 2)).rangePoints([0, getW()]);
+    y = d3.scale.ordinal().domain(d3.range(getLineCount() + 1)).rangePoints([0, getH()]);
 
     // JOIN new data with old elements
-    var dots = this.svg.selectAll('.dot')
-        .data(this.snippet.execution);
+    var dots = svg.selectAll('.dot')
+        .data(snippet.execution);
 
     // EXIT old elements not present in new data
     dots.exit().remove();
 
     // UPDATE old elements present in new data
-    dots.attr('cx', function(d, i) { return this.x(i + 1); }.bind(this))
-        .attr('cy', function(d, i) { return this.y(d) + (this.getH() / this.getLineCount() / 2); }.bind(this));
+    dots.attr('cx', function(d, i) { return x(i + 1); })
+        .attr('cy', function(d, i) { return y(d) + (getH() / getLineCount() / 2); });
 
     // ENTER new elements present in new data
     dots.enter().append('circle')
         .attr('class', function(d, i) { return 'dot' + ' ' + d; })
-        .attr('cx', function(d, i) { return this.x(i + 1); }.bind(this))
-        .attr('cy', function(d, i) { return this.y(d) + (this.getH() / this.getLineCount() / 2); }.bind(this))
+        .attr('cx', function(d, i) { return x(i + 1); })
+        .attr('cy', function(d, i) { return y(d) + (getH() / getLineCount() / 2); })
         .attr('r', 3);
 
     var axis = d3.svg.axis()
-      .scale(this.x)
+      .scale(x)
       .orient('top');
 
-    this.xAxis.call(axis);
+    xAxis.call(axis);
 
     // TODO: Add lines between dots to make order more clear?
+  }
+
+  function getW() {
+    return (snippet.execution.length + 2) * 10;
+  }
+
+  function getH() {
+    return codeWrapper.clientHeight;
+  }
+
+  function getLineCount() {
+    return codeWrapper.querySelector('ol.linenums').children.length;
+  }
+
+  function createMarkup() {
+    wrapper = document.createElement('div');
+    wrapper.classList.add('snippet');
+    codeWrapper = document.createElement('div');
+    codeWrapper.classList.add('code');
+    dataWrapper = document.createElement('div');
+    dataWrapper.classList.add('data');
+    stateWrapper = document.createElement('div');
+    stateWrapper.classList.add('state');
+
+    var pre = document.createElement('pre');
+    pre.classList.add('default', 'linenums', 'prettyprint');
+    code = document.createElement('code');
+
+    pre.appendChild(code);
+    codeWrapper.appendChild(pre);
+    wrapper.appendChild(codeWrapper);
+    wrapper.appendChild(dataWrapper);
+    wrapper.appendChild(stateWrapper);
+    document.querySelector('.snippets').appendChild(wrapper);
   };
 
-  this.getW = function() {
-    return (this.snippet.execution.length + 2) * 10;
-  };
-
-  this.getH = function() {
-    return this.codeWrapper.clientHeight;
-  };
-
-  this.getLineCount = function() {
-    return this.codeWrapper.querySelector('ol.linenums').children.length;
-  };
-
-  this.createMarkup = function() {
-    this.wrapper = document.createElement('div');
-    this.wrapper.classList.add('snippet');
-    this.codeWrapper = document.createElement('div');
-    this.codeWrapper.classList.add('code');
-    this.dataWrapper = document.createElement('div');
-    this.dataWrapper.classList.add('data');
-    this.stateWrapper = document.createElement('div');
-    this.stateWrapper.classList.add('state');
-
-    this.pre = document.createElement('pre');
-    this.pre.classList.add('default', 'linenums', 'prettyprint');
-    this.code = document.createElement('code');
-
-    this.pre.appendChild(this.code);
-    this.codeWrapper.appendChild(this.pre);
-    this.wrapper.appendChild(this.codeWrapper);
-    this.wrapper.appendChild(this.dataWrapper);
-    this.wrapper.appendChild(this.stateWrapper);
-    document.querySelector('.snippets').appendChild(this.wrapper);
-
-  };
-
-  this.addCode = function() {
-    this.code.textContent = js_beautify(this.snippet.code);
+  function addCode() {
+    code.textContent = js_beautify(snippet.code);
     prettyPrint();
   };
 
-  this.addData = function() {
-    this.svg = d3.select(this.dataWrapper)
+  function addData() {
+    svg = d3.select(dataWrapper)
       .append('svg')
         .attr('width', 0)
-        .attr('height', this.getH())
+        .attr('height', getH())
         .style('background-image', 'repeating-linear-gradient(180deg, #F8F8F8, #F8F8F8 ' +
-          (this.getH() / this.getLineCount()) + 'px, #FFFFFF ' + (this.getH() / this.getLineCount()) + 'px, #FFFFFF ' + (this.getH() / this.getLineCount() * 2) + 'px)');
+          (getH() / getLineCount()) + 'px, #FFFFFF ' + (getH() / getLineCount()) + 'px, #FFFFFF ' + (getH() / getLineCount() * 2) + 'px)');
 
-    this.xAxis = this.svg.append("g")
+    xAxis = svg.append("g")
       .attr("class", "axis");
-  };
+  }
 
-  this.addListeners = function() {
-    this.paramSize.addEventListener('input', function(e) {
-      this.snippet.execution.splice(7, 0, 13, 15, 16, 17);
-      this.visualize();
-    }.bind(this));
-  };
+  function addState() {
+    // TODO: Visualize state and behavior
+  }
 };
